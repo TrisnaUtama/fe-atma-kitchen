0cd207bb4227a6153a501e92d3f8bc65286104b2
Iqbal Firmansyah Adinda <134963689+IqbalFirmasnyah@users.noreply.github.com>
2024-05-05 00:41:06 +0700

    benerin code

--- src/containers/PageContentCust.jsx
+++ src/containers/PageContentCust.jsx
@@ -1,11 +1,6 @@
 import Header from "./HeaderCust";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { Suspense, lazy, useState } from "react";
-import { useSelector } from "react-redux";
-import { useEffect, useRef } from "react";
-import SuspenseContent from "./SuspenseContent";
-
 import { Suspense, lazy } from "react";
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
@@ -30,15 +25,6 @@ function PageContent() {
     fetchRoutes();
   })
 
-
-
-  useEffect(() => {
-    const fetchRoutes = async () => {
-      const fetchedRoutes = await fetchData();
-      setRoutes(fetchedRoutes);
-    };
-    fetchRoutes();
-  }, []);
 
 
   // Scroll back to top on new page load
